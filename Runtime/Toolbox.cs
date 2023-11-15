2ef510d76d5414e8b49aa47035b9a90865dc2736
ligofff <liga@ligofff.ru>
2023-11-15 18:56:15 +0300

    Basic toolbox setup

---
+++ Runtime/Toolbox.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Toolbox
+{
+    public IEnumerable<ITool> Tools => _tools.Values;
+    
+    private Dictionary<Type, ITool> _tools = new Dictionary<Type, ITool>();
+
+    public void Add<T>(T toolInstance) where T : ITool
+    {
+        if (_tools.ContainsKey(typeof(T)))
+            throw new Exception($"Toolbox already contains a tool of type {typeof(T)}!");
+
+        _tools[typeof(T)] = toolInstance;
+    }
+
+    public void Remove<T>(T toolboxUnit) where T : ITool
+    {
+        if (!_tools.ContainsKey(typeof(T)))
+            throw new Exception($"Toolbox does not contain a tool of type {typeof(T)}!");
+        
+        _tools.Remove(typeof(T));
+    }
+    
+    public T Get<T>() where T : ITool
+    {
+        if (_tools.TryGetValue(typeof(T), out var tool))
+            return (T)tool;
+        
+        throw new Exception($"Toolbox does not contain a tool of type {typeof(T)}!");
+    }
+}
