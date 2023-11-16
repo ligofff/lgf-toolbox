bb8364a752d34099c7a81eddd8fcdc901ca01d20
ligofff <liga@ligofff.ru>
2023-11-16 12:42:46 +0300

    ToolboxAsset & CustomInspector added

---
+++ Editor/ToolboxAssetInspector.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CustomEditor(typeof(ToolboxAsset))]
+    public class ToolboxAssetInspector : Editor
+    {
+        private SerializedProperty predefinedTools;
+
+        private void OnEnable()
+        {
+            predefinedTools = serializedObject.FindProperty("predefinedTools");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(predefinedTools, true);
+            if (GUILayout.Button("Add Tool", GUILayout.Width(100)))
+            {
+                CreateNewTool();
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void CreateNewTool()
+        {
+            var window = EditorWindow.GetWindow<TypePickerWindow>("Select ITool", true);
+            window.SetCallback(AddToolCallback, target.GetType().GetField("predefinedTools").GetValue(target) as List<ITool>);
+            window.Show();
+        }
+
+        private void AddToolCallback(ITool tool)
+        {
+            predefinedTools.InsertArrayElementAtIndex(predefinedTools.arraySize);
+            var toolProperty = predefinedTools.GetArrayElementAtIndex(predefinedTools.arraySize - 1);
+            toolProperty.managedReferenceValue = tool;
+            serializedObject.ApplyModifiedProperties();
+            SetDirty();
+        }
+
+        private void SetDirty()
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+
+    public class TypePickerWindow : EditorWindow
+    {
+        private System.Action<ITool> callback;
+        private List<ITool> _tools;
+
+        public void SetCallback(System.Action<ITool> callback, List<ITool> tools)
+        {
+            this.callback = callback;
+            _tools = tools;
+        }
+
+        private void OnGUI()
+        {
+            var tools = typeof(ITool).GetImplementedTypes();
+            foreach (var toolType in tools)
+            {
+                var buttonLabel = toolType.Name;
+                if (_tools.Any(tool => tool.GetType() == toolType))
+                {
+                    buttonLabel += " (Exists)";
+                }
+                
+                if (GUILayout.Button(buttonLabel))
+                {
+                    if (_tools.Any(tool => tool.GetType() == toolType))
+                    {
+                        Close();
+                        throw new Exception("Tool already exists!");
+                        return;
+                    }
+                    
+                    var newTool = Activator.CreateInstance(toolType);
+                    callback?.Invoke(newTool as ITool);
+                    Close();
+                }
+            }
+        }
+    }
+
+    public static class TypeExtensions
+    {
+        public static IEnumerable<System.Type> GetImplementedTypes(this System.Type type)
+        {
+            return TypeCache.GetTypesDerivedFrom(typeof(ITool)).Where(t => t.GetInterfaces().Contains(type) && t.IsSerializable);
+        }
+    }
+}
---
+++ Runtime/ToolboxAsset.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Toolbox Asset", menuName = "Ligofff/Toolbox")]
+public class ToolboxAsset : ScriptableObject
+{
+    [SerializeReference]
+    public List<ITool> predefinedTools = new List<ITool>();
+
+    private Toolbox _cachedToolbox;
+
+    public Toolbox GetToolbox()
+    {
+        if (_cachedToolbox != null)
+            return _cachedToolbox;
+            
+        _cachedToolbox = new Toolbox();
+            
+        AddPredefinedTools(_cachedToolbox);
+        AddTools(_cachedToolbox);
+            
+        return _cachedToolbox;
+    }
+
+    public void ResetToolbox()
+    {
+        _cachedToolbox = null;
+    }
+
+    private void AddPredefinedTools(Toolbox toolbox)
+    {
+        foreach (var tool in predefinedTools)
+        {
+            toolbox.Add(tool);
+        }
+    }
+        
+    protected virtual void AddTools(Toolbox toolbox)
+    {
+            
+    }
+}
